07f99d3c91e537e40cb4a0fe714fc2d02af90749
osm3rr <osm3rr@mail.com>
2025-01-11 17:44:47 -0400

    list item done v1

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+    type Item = {
+        title: string;
+        checked?: boolean;
+    }
+    const GroupsItems: Item[] = [
+        { title: 'Leer un libro al mes', checked: false },
+        { title: 'Hacer ejercicio', checked: true },
+        { title: 'Aprender algo nuevo' },
+    ];
+</script>
+
+<template>
+    <ul>
+        <li :key="index" v-for="(element, index) in GroupsItems">
+            <ListItem :isChecked="element.checked">
+                {{ element.title }}
+            </ListItem>
+
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+    li {
+        margin: 0.5rem 0;
+    }
+</style>
