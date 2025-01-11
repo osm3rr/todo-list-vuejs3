72a0f2f8eb141f2d4127e3f0726661e16061bba6
osm3rr <osm3rr@mail.com>
2025-01-11 18:17:09 -0400

    reactive done

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -6,7 +6,11 @@
     
 <template>
     <label :class="{'checked': isChecked}">
-        <input type="checkbox" :checked="isChecked" />
+        <input 
+            type="checkbox" 
+            :checked="isChecked"
+            @change="$emit('updateItem')"
+            />
         <slot></slot>
 
     </label>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,38 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
+    import type { Ref } from 'vue';
     import ListItem from './ListItem.vue';
     type Item = {
         title: string;
         checked?: boolean;
     }
-    const GroupsItems: Item[] = [
+    const GroupsItems: Ref<Item[]> = ref([
         { title: 'Leer un libro al mes', checked: false },
         { title: 'Hacer ejercicio', checked: true },
         { title: 'Aprender algo nuevo' },
-    ];
+    ]);
+
+    const updateItem = (element: Item): void => {
+        const updatedItem = findItemList(element)
+        if (updatedItem) {
+            toggleItemChecked(updatedItem);
+        }
+    }
+
+    const findItemList = (element: Item): Item | undefined => {
+        return GroupsItems.value.find((elementItemInList) => elementItemInList.title === element.title);
+    }
+
+    const toggleItemChecked = (element: Item): void => {
+        element.checked = !element.checked;
+    }
+
 </script>
 
 <template>
     <ul>
         <li :key="index" v-for="(element, index) in GroupsItems">
-            <ListItem :isChecked="element.checked">
+            <ListItem :isChecked="element.checked" @updateItem="() => updateItem(element)">
                 {{ element.title }}
             </ListItem>
 
